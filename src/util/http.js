cead2ba8de47d826c255443d650b04eeb42d720c
Vidhi <mathurvidhi2505@gmail.com>
2024-02-27 21:00:29 +0530

    Till lecture 405

--- src/util/http.js
+++ src/util/http.js
@@ -6,7 +6,7 @@ export default async function fetchEvents({ signal, searchTerm, max }) {
   let url = 'http://localhost:3000/events';
 
   if(searchTerm && max){
-    url += '?search=' + searchTerm + '?max=' + max
+    url += '?search=' + searchTerm + '&max=' + max
   }
 
   else if (searchTerm) {
